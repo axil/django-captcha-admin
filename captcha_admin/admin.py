a1afbb481af1a2856495b7cafde2ed85280e6704
Micah Mangione <mmangione@connectio.us>
2020-05-01 04:31:21 -0700

    Working version for Django 2.2

--- captcha_admin/admin.py
+++ captcha_admin/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
-# from django.contrib.admin import autodiscover
-# from django.contrib.auth.admin import UserAdmin, GroupAdmin
-# from django.contrib.auth.models import User, Group
-
 
 from .forms import AdminAuthenticationForm
+from .mixins import AdminSiteRegistryFix
 
 
-class AdminSite(admin.AdminSite):
+class AdminSite(admin.AdminSite, AdminSiteRegistryFix):
     login_form = AdminAuthenticationForm
     login_template = 'admin/captcha_login.html'
 
 site = AdminSite()
 admin.site = site
-# admin.site.register(Group, GroupAdmin)
-# admin.site.register(User, UserAdmin)
--- captcha_admin/apps.py
+++ captcha_admin/apps.py
@@ -3,6 +3,7 @@ from django.contrib.admin.apps import AdminConfig as _AdminConfig
 
 class AdminConfig(_AdminConfig):
     """
-    Inherit Django AdminConfig. We want the autodiscover feature.
+    Inherit Django AdminConfig. We want the autodiscover feature. Do not define your default_site according to the
+    Django docs. Otherwise, we will end up with a recursive import error when loading this module.
     """
-    default_site = 'captcha_admin.admin.AdminSite'
+    pass
