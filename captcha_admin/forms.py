008c2dac75c193686e4b18f9e6e4ee3c192acfb9
Lev Maximov <lev.maximov@gmail.com>
2020-05-18 22:43:49 +0700

    switched to recaptcha v3

--- captcha_admin/forms.py
+++ captcha_admin/forms.py
@@ -1,7 +1,8 @@
 from django.contrib.admin.forms import AdminAuthenticationForm as _AdminAuthenticationForm
 
 from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV3
 
 
 class AdminAuthenticationForm(_AdminAuthenticationForm):
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
